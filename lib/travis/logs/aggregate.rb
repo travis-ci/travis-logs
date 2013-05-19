625ae6e680bcd6de60ee471388f8972b30c62ad3
Josh Kalderimis <josh.kalderimis@gmail.com>
2013-05-19 14:04:02 +0200

    change travis/log/aggregate.rb to be a class and not a script
    this brings it in line with receive.rb

--- lib/travis/logs/aggregate.rb
+++ lib/travis/logs/aggregate.rb
@@ -2,23 +2,33 @@ require 'travis'
 require 'travis/support'
 require 'core_ext/kernel/run_periodically'
 
-Travis::Database.connect
-Travis::Features.start
-Travis::Notification.setup
-Travis::Exceptions::Reporter.start
+module Travis
+  module Logs
+    class Aggregate
+      def setup
+        Travis::Database.connect
+        Travis::Features.start
+        Travis::Notification.setup
+        Travis::Exceptions::Reporter.start
 
-Travis::Async.enabled = true
-Travis::Async::Sidekiq.setup(Travis.config.redis.url, Travis.config.sidekiq)
+        Travis::Async.enabled = true
+        Travis::Async::Sidekiq.setup(Travis.config.redis.url, Travis.config.sidekiq)
 
-instrumenter = Travis.env == 'production' ? Travis::Instrumentation : Travis::Notification
-instrumenter.setup
+        instrumenter = Travis.env == 'production' ? Travis::Instrumentation : Travis::Notification
+        instrumenter.setup
+      end
 
-def aggregate_logs
-  Travis.run_service(:logs_aggregate)
-rescue Exception => e
-  Travis::Exceptions.handle(e)
+      def run
+        run_periodically(Travis.config.logs.intervals.vacuum || 10) do
+          aggregate_logs if Travis::Features.feature_active?(:log_aggregation)
+        end.join
+      end
+
+      def aggregate_logs
+        Travis.run_service(:logs_aggregate)
+      rescue Exception => e
+        Travis::Exceptions.handle(e)
+      end
+    end
+  end
 end
-
-run_periodically(Travis.config.logs.intervals.vacuum || 10) do
-  aggregate_logs if Travis::Features.feature_active?(:log_aggregation)
-end.join
