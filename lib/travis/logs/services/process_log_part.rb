b74d907cbb168c65548a5a88d553a9e93f045c22
Josh Kalderimis <josh.kalderimis@gmail.com>
2013-05-25 17:20:13 +0200

    tune the metrics a bit more for ProcessLogPart

--- lib/travis/logs/services/process_log_part.rb
+++ lib/travis/logs/services/process_log_part.rb
@@ -7,6 +7,8 @@ module Travis
   module Logs
     module Services
       class ProcessLogPart
+        METRIKS_PREFIX = "logs.process_log_part"
+
         def self.run(payload)
           new(payload).run
         end
@@ -18,35 +20,50 @@ module Travis
         end
 
         def run
-          create_part
-          notify
+          measure do
+            find_or_create_log
+            create_part
+            notify
+          end
         end
 
         private
 
           def create_part
-            measure('logs.update') do
-              Travis.logger.warn "[warn] log.id is #{log.id.inspect} in :logs_append! job_id: #{payload['id']}" if log.id.to_i == 0
-              LogPart.create!(log_id: log.id, content: chars, number: number, final: final?)
-            end
+            valid_log_id?
+            LogPart.create!(log_id: log.id, content: chars, number: number, final: final?)
           rescue ActiveRecord::ActiveRecordError => e
-            Travis.logger.warn "[warn] could not save log in :logs_append job_id: #{payload['id']}: #{e.message}"
+            Travis.logger.warn "[warn] could not save log_park in create_part job_id: #{payload['id']}: #{e.message}"
             Travis.logger.warn e.backtrace
           end
 
+          def valid_log_id?
+            if log.id.to_i == 0
+              Travis.logger.warn "[warn] log.id is #{log.id.inspect} in create_part (job_id: #{payload['id']})" 
+              mark('log.id_invalid')
+            end
+          end
+
           def notify
-            Logs.config.pusher_client["job-#{payload['id']}"].trigger('job:log', pusher_payload)
+            measure('pusher') do
+              Logs.config.pusher_client["job-#{payload['id']}"].trigger('job:log', pusher_payload)
+            end
           rescue => e
-            Metriks.meter('travis.logs.update.notify.errors').mark
             Travis.logger.error("Error notifying of log update: #{e.message} (from #{e.backtrace.first})")
           end
 
           def log
-            @log ||= Log.where(job_id: payload['id']).select(:id).first || create_log
+            @log ||= find_log || create_log
+          end
+          alias_method :find_or_create_log, :log
+
+          def find_log
+            Log.where(job_id: payload['id']).select(:id).first
           end
 
           def create_log
-            Travis.logger.warn "[warn] Had to create a log for job_id: #{payload['id']}!"
+            Travis.logger.warn "Had to create a log for job_id: #{payload['id']}!"
+            mark('log.create')
             Log.create!(job_id: payload['id'])
           end
 
@@ -63,11 +80,21 @@ module Travis
           end
 
           def filter(chars)
-            Coder.clean!(chars.to_s.gsub("\0", '')) # postgres seems to have issues with null chars
+            # postgres seems to have issues with null chars
+            Coder.clean!(chars.to_s.gsub("\0", ''))
           end
 
-          def measure(name, &block)
-            Metriks.timer(name).time(&block)
+          def measure(name=nil, &block)
+            timer_name = [METRIKS_PREFIX, name].compact.join('.')
+            Metriks.timer(timer_name).time(&block)
+          rescue => e
+            failed_name = [name, 'failed'].compact.join('.')
+            mark(failed_name)
+            raise
+          end
+
+          def mark(name)
+            Metriks.meter("#{METRIKS_PREFIX}.#{name}").mark
           end
 
           def pusher_payload
