1b4ce621cc1bab4b0e527ecf17b198d0f90d48f5
Josh Kalderimis <josh.kalderimis@gmail.com>
2013-06-27 21:18:20 +0200

    move the db connection logic into logs
    this allows us to add reaping_frequency to the AR config (http://edgeapi.rubyonrails.org/classes/ActiveRecord/ConnectionAdapters/ConnectionPool.html)

--- lib/travis/logs/helpers/database.rb
+++ lib/travis/logs/helpers/database.rb
@@ -9,7 +9,10 @@ module Travis
         # the only way to preload the columns for AR is to call [model].columns
         def self.setup
           Travis.logger.info('Setting up database connection and preloading model columns')
-          Travis::Database.connect
+          ActiveRecord::Base.default_timezone = :utc
+          ActiveRecord::Base.logger = Travis.logger
+          ActiveRecord::Base.configurations = { Travis.env => Travis.config.database.merge(reaping_frequency: 10) }
+          ActiveRecord::Base.establish_connection(Travis.env)
           Log.columns
           LogPart.columns
         end
