239275cb92ba0d5543f9808003829a9471b92cf5
Henrik Hodne <henrik@hodne.io>
2013-10-31 21:02:10 -0500

    Add purge log service
    The purge log service is responsible for removing the content of logs
    after they have been archived to S3.

---
+++ lib/travis/logs/services/purge_log.rb
@@ -0,0 +1,64 @@
+require "travis/logs/helpers/metrics"
+require "travis/logs/helpers/s3"
+
+module Travis
+  module Logs
+    module Services
+      class PurgeLog
+        include Helpers::Metrics
+
+        METRIKS_PREFIX = "logs.purge"
+
+        def self.metriks_prefix
+          METRIKS_PREFIX
+        end
+
+        def initialize(log_id, storage_service = Helpers::S3.new, database = Travis::Logs.database_connection)
+          @log_id = log_id
+          @database = database
+          @storage_service = storage_service
+        end
+
+        def run
+          if content.nil?
+            unless content_length.nil?
+              @database.mark_archive_verified(@log_id)
+              @database.mark_purged(@log_id)
+            end
+          else
+            if content_length == content.length
+              @database.clear_log_content(@log_id)
+              @database.mark_purged(@log_id)
+            end
+          end
+        end
+
+        private
+
+        def content
+          log[:content]
+        end
+
+        def content_length
+          @storage_service.content_length(log_url)
+        end
+
+        def log
+          unless defined?(@log)
+            @log = @database.log_for_id(@log_id)
+            unless @log
+              Travis.logger.warn("[warn] log with id:#{@log_id} could not be found")
+              mark("log.not_found")
+            end
+          end
+
+          @log
+        end
+
+        def log_url
+          "http://#{Travis.config.s3.hostname}/jobs/#{log[:job_id]}/log.txt"
+        end
+      end
+    end
+  end
+end
---
+++ spec/unit/travis/logs/services/purge_log_spec.rb
@@ -0,0 +1,48 @@
+require "travis/logs"
+require "travis/logs/services/purge_log"
+
+module Travis::Logs::Services
+  describe PurgeLog do
+    context "content is null" do
+      context "log is on S3" do
+        before(:each) do
+          @database = double("database", mark_archive_verified: nil, mark_purged: nil)
+          @storage_service = double("storage", content_length: 1)
+          @log = { id: 1, job_id: 2, content: nil }
+          allow(@database).to receive(:log_for_id).with(1).and_return(@log)
+        end
+
+        it "marks log as archived" do
+          PurgeLog.new(@log[:id], @storage_service, @database).run
+          expect(@database).to have_received(:mark_archive_verified).with(@log[:id])
+        end
+
+        it "marks log as purged" do
+          PurgeLog.new(@log[:id], @storage_service, @database).run
+          expect(@database).to have_received(:mark_purged).with(@log[:id])
+        end
+      end
+    end
+
+    context "content is not null" do
+      context "content length matches S3" do
+        before(:each) do
+          @database = double("database", mark_archive_verified: nil, mark_purged: nil, clear_log_content: nil)
+          @storage_service = double("storage", content_length: 13)
+          @log = { id: 1, job_id: 2, content: "hello, world!" }
+          allow(@database).to receive(:log_for_id).with(1).and_return(@log)
+        end
+
+        it "marks log as purged" do
+          PurgeLog.new(@log[:id], @storage_service, @database).run
+          expect(@database).to have_received(:mark_purged).with(@log[:id])
+        end
+
+        it "clears the log content" do
+          PurgeLog.new(@log[:id], @storage_service, @database).run
+          expect(@database).to have_received(:clear_log_content).with(@log[:id])
+        end
+      end
+    end
+  end
+end
