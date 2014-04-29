3094bd92a465bea073d59543fa2de9a20e57afa5
Henrik Hodne <henrik@hodne.io>
2014-04-28 23:16:50 -0500

    db: fix add_log_remove_data migration

--- db/migrations/20140428122052_add_log_remove_data.rb
+++ db/migrations/20140428122052_add_log_remove_data.rb
@@ -1,6 +1,8 @@
 Sequel.migration do
-  alter_table(:logs) do
-    add_column :removed_by, :integer
-    add_column :removed_at, :timestamp
+  change do
+    alter_table(:logs) do
+      add_column :removed_by, :integer
+      add_column :removed_at, :timestamp
+    end
   end
 end
