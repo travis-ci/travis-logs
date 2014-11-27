7d49d833fdcd7da6adba9cb24853348cb5d4bbb0
Mathias Meyer <meyer@paperplanes.de>
2014-11-27 16:13:27 +0100

    Trim down the initial Sidekiq setup log output.

--- lib/travis/logs/sidekiq.rb
+++ lib/travis/logs/sidekiq.rb
@@ -6,12 +6,12 @@ module Travis
     module Sidekiq
       class << self
         def setup
-          Travis.logger.info('Setting up Sidekiq and the Redis connection')
-          Travis.logger.info("using redis:#{Logs.config.redis.inspect}")
-          Travis.logger.info("using sidekiq:#{Logs.config.sidekiq.inspect}")
           url = Logs.config.redis.url
-          namespace = Logs.config.sidekiq.namespace
+          redis_host = URI.parse(url).host
           pool_size = Logs.config.sidekiq.pool_size
+          namespace = Logs.config.sidekiq.namespace
+
+          Travis.logger.info("Setting up Sidekiq (pool size: #{pool_size}) and Redis (connecting to host #{redis_host})")
           ::Sidekiq.redis = ::Sidekiq::RedisConnection.create({ :url => url, :namespace => namespace, :size => pool_size })
           if Travis.config.log_level == :debug
             ::Sidekiq.logger = Travis.logger
