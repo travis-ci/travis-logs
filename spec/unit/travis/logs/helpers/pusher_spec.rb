486a092a80c17fa3661e2130f38a541779f9dc43
Dan Buch <dan@travis-ci.org>
2015-11-12 13:41:20 -0500

    Add more specs for things and things

---
+++ spec/unit/travis/logs/helpers/pusher_spec.rb
@@ -0,0 +1,20 @@
+require 'travis/logs/helpers/pusher'
+
+class FakePusherChannel
+  def trigger(event, payload)
+    :triggered
+  end
+end
+
+describe Travis::Logs::Helpers::Pusher do
+  subject { described_class.new(fake_pusher_client) }
+  let(:fake_pusher_client) { { 'job-1' => FakePusherChannel.new } }
+
+  it 'exposes a #push convenience method' do
+    expect(subject.push({ 'id' => '1' })).to eql(:triggered)
+  end
+
+  it 'exposes a #pusher_channel_name convenience method' do
+    expect(subject.pusher_channel_name({ 'id' => '42' })).to eql('job-42')
+  end
+end
--- spec/unit/travis/logs/receive/queue_spec.rb
+++ spec/unit/travis/logs/receive/queue_spec.rb
@@ -1,5 +1,11 @@
 require 'travis/logs/receive/queue'
 
+class FakeMarkable
+  def mark
+    :marked
+  end
+end
+
 describe Travis::Logs::Receive::Queue do
   subject { described_class.new('test', handler) }
 
@@ -15,6 +21,19 @@ describe Travis::Logs::Receive::Queue do
     it 'logs the exception' do
       expect(subject).to receive(:log_exception)
       subject.send(:receive, message, { pay: :load, 'uuid' => 'foo' })
+    end
+  end
+
+  context 'when a block given to #smart_retry repeatedly times out' do
+    before do
+      allow(Timeout).to receive(:timeout).and_raise(Timeout::Error)
+      allow(Metriks).to receive(:meter).and_return(FakeMarkable.new)
+    end
+
+    it 're-raises the error' do
+      expect do
+        subject.send(:smart_retry, &:puts)
+      end.to raise_error(Timeout::Error)
     end
   end
 end
