08bee261fc0cbac8670995ceb5e6deb534787218
Henrik Hodne <henrik@travis-ci.com>
2015-11-19 22:13:30 +0100

    database: set updated_at when setting log content

--- lib/travis/logs/helpers/database.rb
+++ lib/travis/logs/helpers/database.rb
@@ -90,7 +90,7 @@ module Travis
 
         def set_log_content(log_id, content)
           delete_log_parts(log_id)
-          @db[:logs].where(id: log_id).update(content: content, aggregated_at: nil, archived_at: nil, archive_verified: nil)
+          @db[:logs].where(id: log_id).update(content: content, aggregated_at: nil, archived_at: nil, archive_verified: nil, updated_at: Time.now.utc)
         end
 
         AGGREGATEABLE_SELECT_SQL = <<-SQL.squish
