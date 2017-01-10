f8505c31233f51e7fbc073a064b39666ae7ed9f0
Buck Doyle <b@chromatin.ca>
2017-01-10 16:35:47 -0500

    Add JSON serialisers to fix emoji rendering (#66)
    Pusher is encoding the JSON itself and mangling emoji in
    the process. We can instead pass it an already-encoded string
    and have more control over that process. JrJackson encodes
    the emoji properly, as does Oj on MRI.
    
    This includes a test for the Pusher helper.

---
+++ spec/unit/travis/logs/helpers/pusher_spec.rb
@@ -0,0 +1,31 @@
+require 'travis/logs'
+require 'travis/logs/helpers/pusher'
+
+module Travis::Logs::Helpers
+  describe Pusher do
+    let(:pusher_client) { double('pusher-client') }
+    let(:pusher_channel) { double('pusher-channel') }
+    let(:pusher_helper) { described_class.new(pusher_client) }
+
+    let(:payload) {
+      {
+        'id' => '1919',
+        'chars' => 'strike! ✊',
+        'number' => '204',
+        'final' => false
+      }
+    }
+
+    before(:each) do
+      allow(pusher_client).to receive(:[]) { pusher_channel }
+      allow(pusher_channel).to receive(:trigger)
+    end
+
+    it 'pushing a payload triggers a job:log message' do
+      pusher_helper.push(payload)
+
+      expect(pusher_client).to have_received(:[]).with('job-1919')
+      expect(pusher_channel).to have_received(:trigger).with('job:log', '{"id":"1919","_log":"strike! ✊","number":"204","final":false}')
+    end
+  end
+end
