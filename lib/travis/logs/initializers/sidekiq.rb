df702021e17624f61a72f714c00e536aaa4f1cee
Dan Buch <dan@travis-ci.org>
2017-04-03 16:10:51 -0400

    Ensure log parts processor is actually loaded

--- lib/travis/logs/initializers/sidekiq.rb
+++ lib/travis/logs/initializers/sidekiq.rb
@@ -1,15 +1,17 @@
 # frozen_string_literal: true
 
-$LOAD_PATH << 'lib'
+libdir = File.expand_path('../../../../', __FILE__)
+$LOAD_PATH.unshift(libdir) unless $LOAD_PATH.include?(libdir)
+
+require 'active_support/core_ext/logger'
+require 'core_ext/hash/deep_symbolize_keys'
 
 require 'travis/logs'
-require 'travis/support'
-require 'travis/support/exceptions/reporter'
 require 'travis/logs/helpers/database'
 require 'travis/logs/helpers/s3'
-require 'active_support/core_ext/logger'
 require 'travis/logs/sidekiq'
-require 'core_ext/hash/deep_symbolize_keys'
+require 'travis/support'
+require 'travis/support/exceptions/reporter'
 
 $stdout.sync = true
 Travis.logger.info('Setting up Sidekiq')
@@ -21,4 +23,5 @@ Travis::Logs::Sidekiq.setup
 
 require 'travis/logs/sidekiq/aggregate'
 require 'travis/logs/sidekiq/archive'
+require 'travis/logs/sidekiq/log_parts'
 require 'travis/logs/sidekiq/purge'
