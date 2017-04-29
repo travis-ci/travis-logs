26af5f63f11fd23a5f840d82e8901afb9890af0e
Dan Buch <dan@travis-ci.org>
2017-04-28 21:16:51 -0400

    Consolidate raven middleware setup
    since it's effectively always on anyway

--- lib/travis/logs.rb
+++ lib/travis/logs.rb
@@ -31,7 +31,6 @@ module Travis
     autoload :Pusher, 'travis/logs/pusher'
     autoload :RedisPool, 'travis/logs/redis_pool'
     autoload :S3, 'travis/logs/s3'
-    autoload :SentryMiddleware, 'travis/logs/sentry_middleware'
     autoload :Services, 'travis/logs/services'
     autoload :Sidekiq, 'travis/logs/sidekiq'
     autoload :UnderMaintenanceError, 'travis/logs/under_maintenance_error'
