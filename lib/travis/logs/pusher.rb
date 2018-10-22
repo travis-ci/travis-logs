60a9edb2a0672a9fe07bf2f5e2fb6b65b3069708
Igor Wiedler <igor@travis-ci.org>
2018-10-22 11:09:55 +0200

    always sample time to first log line

--- lib/travis/logs/pusher.rb
+++ lib/travis/logs/pusher.rb
@@ -25,6 +25,7 @@ module Travis
           Metriks.timer('logs.time_to_first_log_line.pusher').update(elapsed)
           Metriks.timer("logs.time_to_first_log_line.infra.#{meta['infra']}.pusher").update(elapsed)
           Metriks.timer("logs.time_to_first_log_line.queue.#{meta['queue']}.pusher").update(elapsed)
+          Travis::Honeycomb.always_sample!
           Travis::Honeycomb.context.merge({
             time_to_first_log_line_pusher_ms: elapsed * 1000,
             infra: meta['infra'],
