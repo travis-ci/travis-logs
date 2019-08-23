c3817589682bd34794da47a015d81505a454ab3b
Hiro Asari <asari.ruby@gmail.com>
2019-08-23 19:35:47 -0400

    Initial skeleton of log parsing service

---
+++ lib/travis/logs/services/send_timings.rb
@@ -0,0 +1,117 @@
+# frozen_string_literal: true
+
+require 'concurrent'
+require 'date'
+require 'travis/logs'
+
+module Travis
+  module Logs
+    module Services
+      class SendTimings
+        attr_reader :log_id, :database
+
+        private :database
+
+        TIMER_START = /travis_time:start:(?<timer_id>[0-9a-f]+)/
+        TIMER_END = /travis_time:end:(?<timer_id>[0-9a-f]+):(?<info>[^\r]+)\r/
+
+        def self.run
+          new.run
+        end
+
+        def self.send_timings(log_id)
+          new.send_timings(log_id)
+        end
+
+        def initialize(log_id, database: Travis::Logs.database_connection)
+          @log_id   = log_id
+          @database = database
+        end
+
+        def run
+          parsable_log_ids.each do |log_id|
+            send_timings log_id
+          end
+        end
+
+        def send_timings(log_id)
+          timer_stack = []
+          events      = {}
+
+          content.each_line do |l|
+            l.scan(/#{TIMER_START}|#{TIMER_END}/) do |start_timer_id, end_timer_id, info|
+
+              if start_timer_id
+                timer_stack << start_timer_id
+                next
+              end
+
+              if timer_stack.empty?
+                next
+              end
+              unless (last_timer_id = timer_stack.pop) == end_timer_id
+                next
+              end
+
+              # matched TIMER_END regexp, so we have `end_timer_id` and `info` defined
+
+              marker_data = parse_marker_data(info)
+
+              next unless marker_data.key?(:duration) or marker_data.key?(:duration)
+
+              # duration is given by nanoseconds
+              duration_ms = marker_data.delete(:duration).to_i / (10**6)
+
+              event = {
+                duration_ms: duration_ms,
+                log_id: log_id,
+                cmd_start_time: DateTime.strptime(start_timer_id[0..-10], '%s'), # drop last 9 digits to create time in seconds
+                cmd_end_time:   DateTime.strptime(end_timer_id[0..-10],   '%s'), # drop last 9 digits to create time in seconds
+              }.merge(marker_data)
+
+              Travis::Honeycomb.send(event) # but this is not the right client…
+            end
+          end
+
+          def log
+            @log ||= begin
+              log = database.log_for_id(log_id)
+              unless log
+                Travis.logger.warn(
+                  'log not found',
+                  action: 'archive', id: log_id, result: 'not_found'
+                )
+                mark('log.not_found')
+              end
+              log
+            end
+          end
+          alias fetch log
+
+          private
+
+          def content
+            @content ||= log[:content]
+          end
+
+          attr_writer :content
+          private :content
+
+          def parse_marker_data(str)
+            # given a comma-delimited string with each being an equal-delimited
+            # key-value paris, build a hash with the key-value pairs thus specified
+            # with symbols as keys
+            # e.g., 'a=b,c=d' => '{:a=>"b", :c=> "d"}'
+            str.split(',').map {|s| s.split('=',2)}.to_h.transform_keys(&:to_sym)
+          end
+
+          def parsable_log_ids
+            ids = []
+
+            ids
+          end
+        end
+      end
+    end
+  end
+end
---
+++ lib/travis/logs/sidekiq/send_timings.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+
+require 'sidekiq'
+require 'sidekiq/worker'
+
+require 'travis/logs'
+
+module Travis
+  module Logs
+    module Sidekiq
+      class SendTimings
+        include ::Sidekiq::Worker
+
+        sidekiq_options queue: 'send_timing', retry: 3,
+                        unique: :until_and_while_executing
+
+        def perform(log_id)
+          Travis::Honeycomb.context.set('log_id', log_id)
+          send_timings_service.send_timings(log_id)
+        end
+
+        private def send_timings_service
+          @send_timings_service ||= Travis::Logs::Services::Sendtimings.new
+        end
+      end
+    end
+  end
+end
