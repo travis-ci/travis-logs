7bf0a7c12b6f3e2b2a0a7f0ae0641233c4e667e5
Hiro Asari <asari.ruby@gmail.com>
2019-08-29 08:03:42 -0400

    Turn off stylistic warnings

--- lib/travis/logs/services/timing_info.rb
+++ lib/travis/logs/services/timing_info.rb
@@ -43,7 +43,6 @@ module Travis
 
           content.each_line do |l|
             l.scan(/#{TIMER_START}|#{TIMER_END}/) do |start_timer_id, end_timer_id, info|
-
               if start_timer_id
                 timer_stack << start_timer_id
                 next
