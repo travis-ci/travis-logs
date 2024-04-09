45ccc7a2b4c189950b55a696f5b8b9d7ef347377
Murtaza Khan <104836571+murtaza-swati@users.noreply.github.com>
2024-04-09 12:52:51 +0500

    Q3 maintenance (#269)
    * ensuring sharding policy is set in rabbitmq
    
    * rubocop
    
    * sharding update
    
    * removed default api port
    
    * rubocop fix

--- lib/travis/logs/config.rb
+++ lib/travis/logs/config.rb
@@ -10,7 +10,7 @@ module Travis
       define(
         amqp: {
           automatic_recovery: true,
-          recover_from_connection_close: true
+          recover_from_connection_close: true,
         },
         channels_existence_check: true,
         lock: { strategy: :redis, ttl: 150 },
